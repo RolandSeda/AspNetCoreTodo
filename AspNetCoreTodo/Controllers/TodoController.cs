f8d04fe0bd7f450b88ee73c4e316860aaa64351f
RolandSeda <r.seda@web.de>
2019-06-23 16:09:48 +0200

    third commit added todolist

---
+++ AspNetCoreTodo/Controllers/TodoController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspNetCoreTodo.Models;
+using AspNetCoreTodo.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreTodo.Controllers
+{
+    public class TodoController : Controller
+    {
+        private readonly ITodoItemService _todoItemService;
+
+        public TodoController(ITodoItemService todoItemService)
+        {
+            _todoItemService = _todoItemService;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var items = await _todoItemService.GetIncompleteItemsAsync();
+
+            var model = new TodoViewModel()
+            {
+                Items = items
+            };
+
+            return View(model);
+
+            // Put items into a model
+
+            // Render view using the model
+        }
+    }
+}
---
+++ AspNetCoreTodo/Services/FakeTodoItemService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspNetCoreTodo.Models;
+
+namespace AspNetCoreTodo.Services
+{
+    public class FakeTodoItemService : ITodoItemService
+    {
+        Task<TodoItem[]> GetIncompleteItemsAsync()
+        {
+            var item1 = new TodoItem
+            {
+                Title = "Learn ASP:NET Core",
+                DueAt = DateTimeOffset.Now.AddDays(1)
+            };
+            var item2 = new TodoItem
+            {
+                Title = "build awesome apps",
+                DueAt = DateTimeOffset.Now.AddDays(2)
+            };
+
+            return Task.FromResult(new[] { item1, item2 });
+        }
+
+        Task<TodoItem[]> ITodoItemService.GetIncompleteItemsAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
